7bca0435b59f399e5249497205d3fe4347f6f1d6
ericb2020 <ebrauer83@gmail.com>
2021-01-25 14:22:10 -0600

    completed lab

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -2,21 +2,56 @@
 -- For all of these queries, you SHOULD NOT use an id # in a WHERE clause
 
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
-
+    -- SELECT * FROM authors
+    -- JOIN books
+    -- ON authors.id = author_id
+    -- WHERE name = 'George R.R. Martin';
 -- Find all fields (book and author related) for all books written by Milan Kundera.
-
+    -- SELECT * FROM authors
+    -- JOIN books
+    -- ON authors.id = author_id
+    -- WHERE name = 'Milan Kundera';
 -- Find all books written by an author from China or the UK.
-
+    -- SELECT * FROM authors
+    -- JOIN books
+    -- ON authors.id = author_id
+    -- WHERE nationality = 'China'
+    -- OR nationality = 'United Kingdom';
 -- Find out how many books Albert Camus wrote.
-
+    -- SELECT COUNT(title) FROM authors
+    -- JOIN books
+    -- ON authors.id = author_id
+    -- WHERE name = 'Albert Camus';
 -- Find out how many books were written by US authors.
-
+    -- SELECT COUNT(title) FROM authors
+    -- JOIN books
+    -- ON authors.id = author_id
+    -- WHERE nationality = 'United States of America';
 -- Find all books written after 1930 by authors from Argentina.
-
+    -- SELECT * FROM authors
+    -- JOIN books
+    -- ON authors.id = author_id
+    -- WHERE nationality = 'Argentina'
+    -- AND publication_date > 1930;
 -- Find all books written before 1980 by authors not from the US.
-
+    -- SELECT * FROM authors
+    -- JOIN books
+    -- ON authors.id = author_id
+    -- WHERE nationality <> 'United States of America'
+    -- AND publication_date < 1980;
 -- Find all authors whose names start with 'J'.
-
+    -- SELECT * FROM authors
+    -- JOIN books
+    -- ON authors.id = author_id
+    -- WHERE name LIKE 'J%'
 -- Find all books whose titles contain 'the'.
-
+    -- SELECT * FROM authors
+    -- JOIN books
+    -- ON authors.id = author_id
+    -- WHERE title LIKE '% the %'
+    -- OR title LIKE '%The %';
 -- Find all authors who have written books with that start with the letter 'N'.
+    -- SELECT * FROM authors
+    -- JOIN books
+    -- ON authors.id = author_id
+    -- WHERE title LIKE 'N%';
--- basic_queries.sql
+++ basic_queries.sql
@@ -2,30 +2,31 @@
 -- Unless otherwise stated, all queries should return all columns
 
 -- Get all information about all authors
-
+  --SELECT * FROM authors;
 -- Get just the name and birth year of all authors
-
+  --SELECT name, birth_year FROM authors;
 -- Get all authors born in the 20th centruy or later
-
+  --SELECT * FROM authors WHERE birth_year > 1899;
 -- Get all authors born in the USA
-
+  --SELECT * FROM authors WHERE nationality = 'United States of America';
 -- Get all books published on 1985
-
+  --SELECT * FROM books WHERE publication_date = 1985;
 -- Get all books published before 1989
-
+  --SELECT * FROM books WHERE publication_date < 1989;
 -- Get just the title of all books.
-
+  --SELECT title FROM books;
 -- Get just the year that 'A Dance with Dragons' was published
   -- Cry when you realize how long it's been
-
+  --SELECT publication_date FROM books WHERE title = 'A Dance with Dragons';
 -- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
-
+  --SELECT * FROM books WHERE title LIKE '%the%' or title LIKE '%The%';
 -- Add yourself as an author
-
+  --INSERT INTO authors(name, nationality, birth_year) VALUES ('Eric Brauer', 'United States of America', 1983);
 -- Add two books that you'd like to write (you can hard-code your id as the author id)
-
+  --INSERT INTO books (id, title, publication_date, author_id) VALUES (DEFAULT, '2021', 2021,10), (DEFAULT, '2021 Part II: 2022', 2022, 10);
 -- Update one of your books to have a new title
-
+ -- UPDATE books SET title = '2021 Part II: 2021-er' WHERE id = 70;
 -- Delete your books
-
+  --DELETE FROM books WHERE author_id = 10;
 -- Delete your author entry
+  --DELETE FROM authors WHERE name = 'Eric Brauer';
